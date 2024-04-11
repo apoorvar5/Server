0daea8324895df3aa0a003e9da3cb7d043830e6a
apoorvar5 <apoorvrumale@gmail.com>
2024-04-10 21:10:58 -0700

    created player controller

---
+++ Server/Controllers/PlayersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CountryModel;
+using Server.DTO;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController(PlayerSourceContext context) : ControllerBase
+    {
+
+        // GET: api/Players
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayers()
+        {
+            return await context.Players.ToListAsync();
+        }
+
+        [HttpGet("GetClubCount")]
+        public async Task<ActionResult<IEnumerable<ClubCount>>> GetClubCount()
+        {
+            IQueryable<ClubCount> x = context.Players.Select(c =>
+            new ClubCount
+            {
+                PlayerName = c.PlayerName,
+                PlayerId = c.PlayerId,
+                CountClub = c.PlayerClub.Select(avc => avc.ClubId).Distinct().Count(),
+            }
+            );
+            return await x.ToListAsync();
+        }
+    }
+}
---
+++ Server/DTO/ClubCount.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.DTO
+{
+    public class ClubCount
+    {
+        public int PlayerId { get; set; }
+        public required string PlayerName { get; set; }
+        public int CountClub { get; set; }
+    }
+}
